e114300146bada081ea9d6cab24647ef351eb12b
khattam <medhanshkhattar2@gmail.com>
2023-10-29 18:49:12 -0400

    GraphSurfinM1
    DOne

--- src/graphs/AdjacencyListGraph.java
+++ src/graphs/AdjacencyListGraph.java
@@ -1,20 +1,25 @@
 package graphs;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 import java.util.Set;
 
 public class AdjacencyListGraph<T> extends Graph<T> {
 	Map<T,Vertex> keyToVertex;
-	
+	 private int edgeCount;
 	private class Vertex {
 		T key;
 		List<Vertex> successors;
 		List<Vertex> predecessors;
+		
 		
 		Vertex(T key) {
 			this.key = key;
@@ -34,79 +39,159 @@ public class AdjacencyListGraph<T> extends Graph<T> {
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		 return keyToVertex.size();
 	}
 
 	@Override
 	public int numEdges() {
 		// TODO Auto-generated method stub
-		return 0;
+		 return edgeCount;
 	}
 
 	@Override
 	public boolean addEdge(T from, T to) {
-		// TODO Auto-generated method stub
-		return false;
+		Vertex fromVertex = keyToVertex.get(from);
+	    Vertex toVertex = keyToVertex.get(to);
+
+	    // Check if both vertices exist, if not, throw an exception.
+	    if (fromVertex == null || toVertex == null) {
+	        throw new NoSuchElementException("One or both vertices not found");
+	    }
+
+	    // Prevent adding the same edge if it already exists.
+	    if (fromVertex.successors.contains(toVertex)) {
+	        return false; // Edge already exists
+	    }
+
+	    // Add the edge since it doesn't exist.
+	    fromVertex.successors.add(toVertex);
+	    toVertex.predecessors.add(fromVertex); // If the graph is directed
+	    edgeCount++; // increase the count of edges
+	    return true;
 	}
 
 	@Override
 	public boolean hasVertex(T key) {
-		// TODO Auto-generated method stub
-		return false;
+		return keyToVertex.containsKey(key);
 	}
 
 	@Override
 	public boolean hasEdge(T from, T to) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return false;
+		 Vertex fromVertex = keyToVertex.get(from);
+	        Vertex toVertex = keyToVertex.get(to);
+	        if (fromVertex == null || toVertex == null) {
+	            throw new NoSuchElementException("Vertex not found");
+	        }
+	        return fromVertex.successors.contains(toVertex);
 	}
 
 	@Override
 	public boolean removeEdge(T from, T to) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return false;
+		Vertex fromVertex = keyToVertex.get(from);
+        Vertex toVertex = keyToVertex.get(to);
+        if (fromVertex == null || toVertex == null) {
+            throw new NoSuchElementException("Vertex not found");
+        }
+        if (fromVertex.successors.remove(toVertex)) {
+            toVertex.predecessors.remove(fromVertex);
+            edgeCount--;
+            return true;
+        }
+        return false; // Edge not present
 	}
 
 	@Override
 	public int outDegree(T key) {
 		// TODO Auto-generated method stub
-		return 0;
+		  Vertex vertex = keyToVertex.get(key);
+	        if (vertex == null) {
+	            throw new NoSuchElementException("Vertex not found");
+	        }
+	        return vertex.successors.size();
 	}
 
 	@Override
 	public int inDegree(T key) {
 		// TODO Auto-generated method stub
-		return 0;
+		 Vertex vertex = keyToVertex.get(key);
+	        if (vertex == null) {
+	            throw new NoSuchElementException("Vertex not found");
+	        }
+	        return vertex.predecessors.size();
 	}
 
 	@Override
 	public Set<T> keySet() {
 		// TODO Auto-generated method stub
-		return null;
+		return keyToVertex.keySet();
 	}
 
 	@Override
 	public Set<T> successorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
+		  Vertex vertex = keyToVertex.get(key);
+	        if (vertex == null) {
+	            throw new NoSuchElementException("Vertex not found");
+	        }
+	        Set<T> successors = new HashSet<>();
+	        for (Vertex v : vertex.successors) {
+	            successors.add(v.key);
+	        }
+	        return successors;
 	}
 
 	@Override
 	public Set<T> predecessorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
+		Vertex vertex = keyToVertex.get(key);
+        if (vertex == null) {
+            throw new NoSuchElementException("Vertex not found");
+        }
+        Set<T> predecessors = new HashSet<>();
+        for (Vertex v : vertex.predecessors) {
+            predecessors.add(v.key);
+        }
+        return predecessors;
 	}
 
 	@Override
 	public Iterator<T> successorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
+		 Vertex vertex = keyToVertex.get(key);
+	        if (vertex == null) {
+	            throw new NoSuchElementException("Vertex not found");
+	        }
+	        return new Iterator<T>() {
+	            private final Iterator<Vertex> it = vertex.successors.iterator();
+
+	            @Override
+	            public boolean hasNext() {
+	                return it.hasNext();
+	            }
+
+	            @Override
+	            public T next() {
+	                return it.next().key;
+	            }
+	        };
 	}
 
 	@Override
 	public Iterator<T> predecessorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
+		 Vertex vertex = keyToVertex.get(key);
+		    if (vertex == null) {
+		        throw new NoSuchElementException("Vertex not found");
+		    }
+		    return new Iterator<T>() {
+		        private final Iterator<Vertex> it = vertex.predecessors.iterator();
+
+		        @Override
+		        public boolean hasNext() {
+		            return it.hasNext();
+		        }
+
+		        @Override
+		        public T next() {
+		            return it.next().key;
+		        }
+		    };
 	}
 
 	@Override
@@ -117,8 +202,42 @@ public class AdjacencyListGraph<T> extends Graph<T> {
 
 	@Override
 	public List<T> shortestPath(T startLabel, T endLabel) {
-		// TODO Auto-generated method stub
-		return null;
+		  if (!hasVertex(startLabel) || !hasVertex(endLabel)) {
+		        throw new NoSuchElementException("Start or end vertex not found");
+		    }
+
+		    Queue<T> queue = new LinkedList<>();
+		    Map<T, T> predecessors = new HashMap<>();
+		    Set<T> visited = new HashSet<>();
+		    queue.add(startLabel);
+		    visited.add(startLabel);
+		    boolean pathExists = false;
+
+		    while (!queue.isEmpty()) {
+		        T current = queue.poll();
+		        if (current.equals(endLabel)) {
+		            pathExists = true;
+		            break;
+		        }
+
+		        for (T neighbor : successorSet(current)) {
+		            if (!visited.contains(neighbor)) {
+		                predecessors.put(neighbor, current);
+		                visited.add(neighbor);
+		                queue.add(neighbor);
+		            }
+		        }
+		    }
+
+		    if (!pathExists) {
+		        return Collections.emptyList(); // or throw an appropriate exception.
+		    }
+
+		    List<T> path = new LinkedList<>();
+		    for (T at = endLabel; at != null; at = predecessors.get(at)) {
+		        path.add(0, at);
+		    }
+		    return path;
 	}
 
 }
--- src/graphs/AdjacencyMatrixGraph.java
+++ src/graphs/AdjacencyMatrixGraph.java
@@ -2,101 +2,150 @@ package graphs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
+
+
 public class AdjacencyMatrixGraph<T> extends Graph<T> {
-	Map<T,Integer> keyToIndex;
-	List<T> indexToKey;
-	int[][] matrix;
+	private Map<T,Integer> keyToIndex;
+	private List<T> indexToKey;
+	private int[][] matrix;
+	private int vertexCount;
+	private int edgeCount;
 	
 	AdjacencyMatrixGraph(Set<T> keys) {
 		int size = keys.size();
-		this.keyToIndex = new HashMap<T,Integer>();
-		this.indexToKey = new ArrayList<T>();
+		this.keyToIndex = new HashMap<>();
+		this.indexToKey = new ArrayList<>();
 		this.matrix = new int[size][size];
-		// need to populate keyToIndex and indexToKey with info from keys
+		for (T key : keys) {
+			this.keyToIndex.put(key, this.vertexCount);
+			this.indexToKey.add(key);
+			this.vertexCount++;
+		}
 	}
 	
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.vertexCount;
 	}
 
 	@Override
 	public int numEdges() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.edgeCount;
 	}
 
 	@Override
-	public boolean addEdge(T from, T to) {
-		// TODO Auto-generated method stub
+	public boolean addEdge(T from, T to) throws NoSuchElementException {
+		if (!this.keyToIndex.containsKey(from)) throw new NoSuchElementException("Did not find 'from' vertex");
+		if (!this.keyToIndex.containsKey(to)) throw new NoSuchElementException("Did not find 'to' vertex");
+		int fromIndex = this.keyToIndex.get(from), toIndex = this.keyToIndex.get(to);
+		if (this.matrix[fromIndex][toIndex] == 0) {
+			this.matrix[fromIndex][toIndex] = 1;
+			this.edgeCount++;
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public boolean hasVertex(T key) {
-		// TODO Auto-generated method stub
-		return false;
+		return this.keyToIndex.containsKey(key);
 	}
 
 	@Override
 	public boolean hasEdge(T from, T to) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return false;
+		if (!this.keyToIndex.containsKey(from)) throw new NoSuchElementException("Did not find 'from' vertex");
+		if (!this.keyToIndex.containsKey(to)) throw new NoSuchElementException("Did not find 'to' vertex");
+		int fromIndex = this.keyToIndex.get(from), toIndex = this.keyToIndex.get(to);
+		return this.matrix[fromIndex][toIndex] == 1;
 	}
 
 	@Override
 	public boolean removeEdge(T from, T to) throws NoSuchElementException {
-		// TODO Auto-generated method stub
+		if (!this.keyToIndex.containsKey(from)) throw new NoSuchElementException("Did not find 'from' vertex");
+		if (!this.keyToIndex.containsKey(to)) throw new NoSuchElementException("Did not find 'to' vertex");
+		int fromIndex = this.keyToIndex.get(from), toIndex = this.keyToIndex.get(to);
+		if (this.matrix[fromIndex][toIndex] == 1) {
+			this.matrix[fromIndex][toIndex] = 0;
+			this.edgeCount--;
+			return true;
+		}
 		return false;
 	}
 
 	@Override
-	public int outDegree(T key) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int outDegree(T key) throws NoSuchElementException {
+		try {
+			int from = this.keyToIndex.get(key);
+			int edges = 0;
+			for (int to = 0; to < this.vertexCount; to++) if (this.matrix[from][to] == 1) edges++;
+			return edges;
+		}
+		catch (NullPointerException e) { throw new NoSuchElementException("Did not find 'key' vertex"); }
 	}
 
 	@Override
-	public int inDegree(T key) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int inDegree(T key) throws NoSuchElementException {
+		try {
+			int to = this.keyToIndex.get(key);
+			int edges = 0;
+			for (int from = 0; from < this.vertexCount; from++) if (this.matrix[from][to] == 1) edges++;
+			return edges;
+		}
+		catch (NullPointerException e) { throw new NoSuchElementException("Did not find 'key' vertex"); }
 	}
 
 	@Override
 	public Set<T> keySet() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.keyToIndex.keySet();
 	}
 
 	@Override
-	public Set<T> successorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<T> successorSet(T key) throws NoSuchElementException {
+		 // Check if the vertex exists
+	    if (!this.keyToIndex.containsKey(key)) {
+	        throw new NoSuchElementException("Did not find 'key' vertex");
+	    }
+
+	    Set<T> set = new HashSet<>();
+	    int fromIndex = this.keyToIndex.get(key);
+	    
+	    for (int toIndex = 0; toIndex < this.vertexCount; toIndex++) {
+	        if (this.matrix[fromIndex][toIndex] == 1) {
+	            set.add(this.indexToKey.get(toIndex));
+	        }
+	    }
+	    return set;
 	}
 
 	@Override
-	public Set<T> predecessorSet(T key) {
-		// TODO Auto-generated method stub
-		return null;
+	public Set<T> predecessorSet(T key) throws NoSuchElementException {
+		try {
+			Set<T> set = new HashSet<>();
+			int to = this.keyToIndex.get(key);
+			for (int from = 0; from < this.vertexCount; from++) if (this.matrix[from][to] == 1) set.add(this.indexToKey.get(from));
+			return set;
+		}
+		catch (NullPointerException e) { throw new NoSuchElementException("Did not find 'key' vertex"); }
 	}
 
 	@Override
-	public Iterator<T> successorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterator<T> successorIterator(T key) throws NoSuchElementException {
+		try { return new EdgeIterator(this.indexToKey, this.matrix, this.keyToIndex.get(key), true); }
+		catch (NullPointerException e) { throw new NoSuchElementException("Did not find 'key' vertex"); }
 	}
 
 	@Override
-	public Iterator<T> predecessorIterator(T key) {
-		// TODO Auto-generated method stub
-		return null;
+	public Iterator<T> predecessorIterator(T key) throws NoSuchElementException {
+		try { return new EdgeIterator(this.indexToKey, this.matrix, this.keyToIndex.get(key), false); }
+		catch (NullPointerException e) { throw new NoSuchElementException("Did not find 'key' vertex"); }
 	}
 
 	@Override
@@ -109,6 +158,35 @@ public class AdjacencyMatrixGraph<T> extends Graph<T> {
 	public List<T> shortestPath(T startLabel, T endLabel) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private class EdgeIterator implements Iterator<T> {
+		List<T> indexToKey;
+		int[][] matrix;
+		int vertexCount;
+		int from;
+		int to = -1;
+		boolean successor;
+
+		EdgeIterator(List<T> indexToKey, int[][] matrix, int from, boolean successor) {
+			this.indexToKey = indexToKey;
+			this.matrix = matrix;
+			this.vertexCount = this.matrix.length;
+			this.from = from;
+			this.successor = successor;
+		}
+
+		@Override
+		public boolean hasNext() {
+			for (int t = this.to + 1; t < this.vertexCount; t++) if ((this.successor ? this.matrix[from][t] : this.matrix[t][from]) == 1) return true;
+			return false;
+		}
+
+		@Override
+		public T next() {
+			for (int t = this.to + 1; t < this.vertexCount; t++) if ((this.successor ? this.matrix[from][t] : this.matrix[t][from]) == 1) { this.to = t; break; }
+			return this.indexToKey.get(this.to);
+		}
 	}
 
 }
